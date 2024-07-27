a2891a7f1b604b9cf68b1dda5ac4027b02b954d6
HaoyanZang <3030499547@qq.com>
2024-07-27 18:35:11 +0800

    Add the database, interfaces and others.

---
+++ backend/flavorDash-common/src/main/java/com/flavorDash/exception/UserNotLoginException.java
@@ -0,0 +1,12 @@
+package com.flavorDash.exception;
+
+public class UserNotLoginException extends BaseException {
+
+    public UserNotLoginException() {
+    }
+
+    public UserNotLoginException(String msg) {
+        super(msg);
+    }
+
+}
---
+++ backend/flavorDash-server/src/main/java/com/flavorDash/controller/admin/EmployeeController.java
@@ -0,0 +1,74 @@
+package com.flavorDash.controller.admin;
+
+import com.flavorDash.constant.JwtClaimsConstant;
+import com.flavorDash.dto.EmployeeLoginDTO;
+import com.flavorDash.entity.Employee;
+import com.flavorDash.properties.JwtProperties;
+import com.flavorDash.result.Result;
+import com.flavorDash.service.EmployeeService;
+import com.flavorDash.utils.JwtUtil;
+import com.flavorDash.vo.EmployeeLoginVO;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 员工管理
+ */
+@RestController
+@RequestMapping("/admin/employee")
+@Slf4j
+public class EmployeeController {
+
+    @Autowired
+    private EmployeeService employeeService;
+    @Autowired
+    private JwtProperties jwtProperties;
+
+    /**
+     * 登录
+     *
+     * @param employeeLoginDTO
+     * @return
+     */
+    @PostMapping("/login")
+    public Result<EmployeeLoginVO> login(@RequestBody EmployeeLoginDTO employeeLoginDTO) {
+        log.info("员工登录：{}", employeeLoginDTO);
+
+        Employee employee = employeeService.login(employeeLoginDTO);
+
+        //登录成功后，生成jwt令牌
+        Map<String, Object> claims = new HashMap<>();
+        claims.put(JwtClaimsConstant.EMP_ID, employee.getId());
+        String token = JwtUtil.createJWT(
+                jwtProperties.getAdminSecretKey(),
+                jwtProperties.getAdminTtl(),
+                claims);
+
+        EmployeeLoginVO employeeLoginVO = EmployeeLoginVO.builder()
+                .id(employee.getId())
+                .userName(employee.getUsername())
+                .name(employee.getName())
+                .token(token)
+                .build();
+
+        return Result.success(employeeLoginVO);
+    }
+
+    /**
+     * 退出
+     *
+     * @return
+     */
+    @PostMapping("/logout")
+    public Result<String> logout() {
+        return Result.success();
+    }
+
+}
